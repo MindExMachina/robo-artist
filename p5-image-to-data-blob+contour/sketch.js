08f6d2a003b17416fb711fbd83904f09688ea049
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-05-10 16:27:48 -0400

    Adding contour to p5 webcam sketch.

---
+++ p5-image-to-data-blob+contour/sketch.js
@@ -0,0 +1,26 @@
+var capture;
+var img;
+
+function setup() {
+    createCanvas(390, 240);
+    capture = createCapture(VIDEO);
+    capture.size(320, 240);
+    capture.hide();
+}
+
+function draw() {
+    //background(240);
+    // if we access loadPixels once we stop the capture
+    // we need to re-access at every frame
+    image(capture.loadPixels(), 0, 0, 320, 240);
+    //filter('INVERT');
+}
+
+function captureDataURL() {
+    return capture.loadPixels().canvas.toDataURL();
+}
+
+function setDat() {
+    img = document.getElementById('image');
+    img.src = captureDataURL();
+}
